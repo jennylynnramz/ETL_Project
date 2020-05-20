f52ded13b88b90883d219f3a6805255c08fb7340
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-05-19 21:23:28 -0700

    updating SQL file to include created tables

--- ETL_SQL.sql
+++ ETL_SQL.sql
@@ -1,2 +1,24 @@
 DROP TABLE IF EXISTS happiness;
 DROP TABLE IF EXISTS lockdown;
+
+CREATE TABLE happiness (
+	country_name VARCHAR PRIMARY KEY,
+	regional_indicator VARCHAR,
+	logged_gdp_per_capita FLOAT,
+	social_support FLOAT,
+	healthy_life_expectancy FLOAT,
+	freedom_to_make_life_choices FLOAT,
+	generosity FLOAT,
+	perceptions_of_corruption FLOAT
+);
+
+SELECT * FROM happiness;
+
+CREATE TABLE lockdown (
+	country VARCHAR PRIMARY KEY,
+	province VARCHAR,
+	date DATE,
+	type VARCHAR	
+);
+
+SELECT * FROM lockdown
